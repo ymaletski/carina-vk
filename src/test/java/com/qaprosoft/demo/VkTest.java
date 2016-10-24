f7e05c152496729ddffbeb66fe684f998c851f65
ymaletski <yura.maletski@mail.ru>
2016-10-24 21:34:49 +0300

    v 1.2

--- src/test/java/com/qaprosoft/demo/VkTest.java
+++ src/test/java/com/qaprosoft/demo/VkTest.java
@@ -4,7 +4,12 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import com.qaprosoft.carina.core.foundation.UITest;
+import com.qaprosoft.carina.core.foundation.dataprovider.annotations.XlsDataSourceParameters;
 import com.qaprosoft.carina.core.foundation.utils.R;
+import com.qaprosoft.carina.core.foundation.utils.ownership.MethodOwner;
+import com.qaprosoft.demo.gui.gsmarena.pages.BrandModelsPage;
+import com.qaprosoft.demo.gui.gsmarena.pages.HomePage;
+import com.qaprosoft.demo.gui.gsmarena.pages.ModelInfoPage;
 import com.qaprosoft.demo.gui.vk.pages.EditContactsPage;
 import com.qaprosoft.demo.gui.vk.pages.EditPage;
 import com.qaprosoft.demo.gui.vk.pages.FriendsPage;
@@ -14,7 +19,25 @@ import com.qaprosoft.demo.gui.vk.pages.NewsPage;
 
 public class VkTest extends UITest {
 	
-	@Test
+	@Test(dataProvider = "SingleDataProvider", description = "JIRA#AUTO-10001")
+	@XlsDataSourceParameters(path = "xls/testdata.xlsx", sheet = "numbers", dsUid = "TUID", dsArgs = "mobile, home")
+	public void editContactsManyTimes(String mobile, String home) {
+		LogInPage logInPage = new LogInPage(getDriver());
+		logInPage.open();
+		Assert.assertTrue(logInPage.isPageOpened(), "LogIn page is not opened.");
+		NewsPage newsPage = logInPage.logIn();
+		Assert.assertTrue(newsPage.isPageOpened(), "News page is not opened.");
+		EditPage editPage = newsPage.getProfileMenu().openEditPage();
+		Assert.assertTrue(editPage.isPageOpened(), "Edit page is not opened.");
+		EditContactsPage editContactsPage = editPage.openEditContactsPage();
+		Assert.assertTrue(editContactsPage.isPageOpened(), 
+				"Edit contacts page is not opened.");
+		editContactsPage.editMobileTel(mobile);
+		editContactsPage.editHomeTel(home);
+		editContactsPage.getProfileMenu().logOut();
+	}
+	
+	@Test(description = "JIRA#AUTO-10002")
 	public void editContacts() {
 		LogInPage logInPage = new LogInPage(getDriver());
 		logInPage.open();
@@ -31,7 +54,7 @@ public class VkTest extends UITest {
 		editContactsPage.getProfileMenu().logOut();
 	}
 	
-	@Test
+	@Test(description = "JIRA#AUTO-10003")
 	public void findFriendsByName() {
 		LogInPage logInPage = new LogInPage(getDriver());
 		logInPage.open();
